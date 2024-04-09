e326529fa9a00c9886651b753583f1dad5495596
cyetran <156265100+cyetran@users.noreply.github.com>
2024-04-08 22:51:29 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function initMap(){
   center: {lat: -34.397, lng: 150.644},
   zoom: 14
 });
+  
 const marker = new AdvancedMarkerElement({
   map,
   position: { lat: 37.4239163, lng: -122.0947209 },
