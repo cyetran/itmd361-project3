79b8a3180ce30060905c94b0c67abb86a5ac3895
cyetran <156265100+cyetran@users.noreply.github.com>
2024-04-08 23:32:24 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,32 @@
+const citymap = {
+  chicago: {
+    center: { lat: 41.878, lng: -87.629 },
+    population: 2714856,
+  },
+};
+
 var map;
 async function initMap(){
   map = new google.maps.Map(document.getElementById('map'), {
-  center: {lat: 41.834875, lng: -87.628093},
-  zoom: 14
+    center: {lat: 41.834875, lng: -87.628093},
+    zoom: 15,
+    mapTypeId: "terrain",
   }
   );
+
+  for (const city in citymap) {
+    // Add the circle for this city to the map.
+    const cityCircle = new google.maps.Circle({
+      strokeColor: "#FF0000",
+      strokeOpacity: 0.8,
+      strokeWeight: 2,
+      fillColor: "#FF0000",
+      fillOpacity: 0.35,
+      map,
+      center: citymap[city].center,
+      radius: Math.sqrt(citymap[city].population) * 100,
+    });
+  }
 }
 
 initMap();
