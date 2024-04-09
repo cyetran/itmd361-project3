80a4c7820b27964e873b4f3980b9458377843042
cyetran <156265100+cyetran@users.noreply.github.com>
2024-04-08 23:24:27 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,8 @@ async function initMap(){
   map = new google.maps.Map(document.getElementById('map'), {
   center: {lat: 41.834875, lng: -87.628093},
   zoom: 14
-});
-  
-const marker = new AdvancedMarkerElement({
-  map,
-  position: { lat: 37.4239163, lng: -122.0947209 },
-  });
+  }
+  );
 }
 
 initMap();
