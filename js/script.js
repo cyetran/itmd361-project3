ec472264935b41a9993f3b753d56ec1b5f943968
cyetran <156265100+cyetran@users.noreply.github.com>
2024-04-09 00:05:58 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ async function initMap(){
       strokeOpacity: 0.8,
       strokeWeight: 2,
       fillColor: "#FF0000",
-      fillOpacity: 0.35,
+      fillOpacity: 0.15,
       map,
       center: citymap[city].center,
       radius: Math.sqrt(citymap[city].population) * 100,
