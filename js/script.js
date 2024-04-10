ad2a9dfd7e502351ebc875454d61362df3dc3509
cyetran <156265100+cyetran@users.noreply.github.com>
2024-04-09 21:24:29 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,9 +5,9 @@ const citymap = {
   },
 };
 
-
+var map;
 function initMap(){
-  const { map } = await google.maps.importLibrary("maps");
+  const { Map } = await google.maps.importLibrary("maps");
   const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 41.834875, lng: -87.628093},
